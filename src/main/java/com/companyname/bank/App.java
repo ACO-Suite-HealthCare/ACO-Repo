60e98fb0a7acdd9b97312ad4ea6217f37cba7831
srini <srini.gmail.com>
2021-04-16 13:01:41 +0530

    updated App.java file

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! change new-check" );
+        System.out.println( "Hello World mama! change new-check" );
         System.out.println( "Srinivas Gunnabathula M Durga Srinivasa rao" );
     }
 }
